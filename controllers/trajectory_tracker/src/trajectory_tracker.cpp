e8b1cce9ee287e51e32e5098a640e5f7d09bfa7f
justinthomas <justinthomas@jtwebs.net>
2014-09-01 00:38:48 -0400

    Changes trajectory_tracker::GoalCommand to quadrotor_msgs::PositionCommand

--- controllers/trajectory_tracker/src/trajectory_tracker.cpp
+++ controllers/trajectory_tracker/src/trajectory_tracker.cpp
@@ -1,11 +1,10 @@
 #include <ros/ros.h>
 #include <controllers_manager/Controller.h>
 #include <nav_msgs/Odometry.h>
-#include <quadrotor_msgs/PositionCommand.h>
 #include <geometry_msgs/Vector3.h>
 #include <Eigen/Geometry>
 #include <tf/transform_datatypes.h>
-#include <trajectory_tracker/GoalCommand.h>
+#include <quadrotor_msgs/PositionCommand.h>
 #include <math.h>
 
 class TrajectoryTracker : public controllers_manager::Controller
@@ -20,16 +19,14 @@ class TrajectoryTracker : public controllers_manager::Controller
   const quadrotor_msgs::PositionCommand::Ptr update(const nav_msgs::Odometry::ConstPtr &msg);
 
  private:
-  void goal_callback(const trajectory_tracker::GoalCommand::ConstPtr &msg);
+  void goal_callback(const quadrotor_msgs::PositionCommand::ConstPtr &msg);
+  quadrotor_msgs::PositionCommand::Ptr cmd_;
 
   ros::Subscriber sub_goal_;
   ros::ServiceServer srv_param_;
   bool pos_set_, goal_set_;
   bool active_;
 
-  Eigen::Vector3f pos_, vel_;
-  Eigen::Vector4f xdes_, vdes_, ades_; //  goal_
-  float yaw_;
   double kx_[3], kv_[3];
 };
 
@@ -77,51 +74,25 @@ void TrajectoryTracker::Deactivate(void)
 
 const quadrotor_msgs::PositionCommand::Ptr TrajectoryTracker::update(const nav_msgs::Odometry::ConstPtr &msg)
 {
-  pos_(0) = msg->pose.pose.position.x;
-  pos_(1) = msg->pose.pose.position.y;
-  pos_(2) = msg->pose.pose.position.z;
-  yaw_ = tf::getYaw(msg->pose.pose.orientation);
-  
-  vel_(0) = msg->twist.twist.linear.x;
-  vel_(1) = msg->twist.twist.linear.y;
-  vel_(2) = msg->twist.twist.linear.z;
-  
   pos_set_ = true;
 
   if(!active_)
     return quadrotor_msgs::PositionCommand::Ptr();
+  
+  // Update the header
+  cmd_->header.stamp = ros::Time::now();
+  cmd_->header.frame_id = msg->header.frame_id;
+ 
+  // Update the gains.  Note, we may want to have these programmed into the trajectory 
+  cmd_->kx[0] = kx_[0], cmd_->kx[1] = kx_[1], cmd_->kx[2] = kx_[2];
+  cmd_->kv[0] = kv_[0], cmd_->kv[1] = kv_[1], cmd_->kv[2] = kv_[2];
 
-  quadrotor_msgs::PositionCommand::Ptr cmd(new quadrotor_msgs::PositionCommand);
-  cmd->header.stamp = ros::Time::now();
-  cmd->header.frame_id = msg->header.frame_id;
-  cmd->yaw = xdes_(3);
-  cmd->yaw_dot = vdes_(3);
-  cmd->kx[0] = kx_[0], cmd->kx[1] = kx_[1], cmd->kx[2] = kx_[2];
-  cmd->kv[0] = kv_[0], cmd->kv[1] = kv_[1], cmd->kv[2] = kv_[2];
-
-  cmd->position.x = xdes_(0), cmd->position.y = xdes_(1), cmd->position.z = xdes_(2);
-  cmd->velocity.x = vdes_(0), cmd->velocity.y = vdes_(1), cmd->velocity.z = vdes_(2);
-  cmd->acceleration.x = ades_(0), cmd->acceleration.y = ades_(1), cmd->acceleration.z = ades_(2);
-  return cmd;
+  return cmd_;
 }
 
-void TrajectoryTracker::goal_callback(const trajectory_tracker::GoalCommand::ConstPtr &msg)
+void TrajectoryTracker::goal_callback(const quadrotor_msgs::PositionCommand::ConstPtr &msg)
 {
-  xdes_(0) = msg->pos.x;
-  xdes_(1) = msg->pos.y;
-  xdes_(2) = msg->pos.z;
-  xdes_(3) = msg->pos.yaw;
-
-  vdes_(0) = msg->vel.x;
-  vdes_(1) = msg->vel.y;
-  vdes_(2) = msg->vel.z;
-  vdes_(3) = msg->vel.yaw;
-
-  ades_(0) = msg->acc.x;
-  ades_(1) = msg->acc.y;
-  ades_(2) = msg->acc.z;
-  ades_(3) = msg->acc.yaw;
-
+  cmd_.reset(new quadrotor_msgs::PositionCommand(*msg));
   goal_set_ = true;
 }
 
