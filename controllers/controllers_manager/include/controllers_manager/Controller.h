111fc0a34b482114762bee9b11939957feea866e
Kartik Mohta <kartikmohta@gmail.com>
2014-09-24 22:39:55 -0400

    controllers_manager: Small cleanup of the Controller abstract class

--- controllers/controllers_manager/include/controllers_manager/Controller.h
+++ controllers/controllers_manager/include/controllers_manager/Controller.h
@@ -11,18 +11,12 @@ namespace controllers_manager
 class Controller
 {
  public:
-  virtual ~Controller(void) = 0;
-
   virtual void Initialize(const ros::NodeHandle &nh) = 0;
   virtual bool Activate(void) = 0;
   virtual void Deactivate(void) = 0;
 
   virtual const quadrotor_msgs::PositionCommand::Ptr update(const nav_msgs::Odometry::ConstPtr &msg) = 0;
 };
-
-Controller::~Controller(void)
-{
-}
 
 }
 
