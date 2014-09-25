2fdb34583c98b7a93ef989d8e2d242891144030f
Kartik Mohta <kartikmohta@gmail.com>
2014-09-24 22:36:22 -0400

    Making the NullTracker minimal

--- controllers/null_tracker/src/null_tracker.cpp
+++ controllers/null_tracker/src/null_tracker.cpp
@@ -1,41 +1,27 @@
 #include <ros/ros.h>
 #include <controllers_manager/Controller.h>
-#include <quadrotor_msgs/PositionCommand.h>
 
 class NullTracker : public controllers_manager::Controller
 {
  public:
-  NullTracker(void);
-
   void Initialize(const ros::NodeHandle &nh);
   bool Activate(void);
   void Deactivate(void);
 
   const quadrotor_msgs::PositionCommand::Ptr update(const nav_msgs::Odometry::ConstPtr &msg);
-
- private:
-  bool active_;
 };
-
-NullTracker::NullTracker(void) :
-    active_(false)
-{
-}
 
 void NullTracker::Initialize(const ros::NodeHandle &nh)
 {
-  ros::NodeHandle priv_nh(nh, "null_tracker");
 }
 
 bool NullTracker::Activate(void)
 {
-  active_ = true;
-  return active_;
+  return true;
 }
 
 void NullTracker::Deactivate(void)
 {
-  active_ = false;
 }
 
 const quadrotor_msgs::PositionCommand::Ptr NullTracker::update(const nav_msgs::Odometry::ConstPtr &msg)
