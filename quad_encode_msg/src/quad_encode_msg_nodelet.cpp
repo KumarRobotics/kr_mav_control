48c4613301b98f13733efdab1ac8354590920fd0
Justin Thomas <justinthomas@jtwebs.net>
2014-12-20 10:38:59 -0500

    Adds PWMCommand and encoder to quadrotor_control

--- quad_encode_msg/src/quad_encode_msg_nodelet.cpp
+++ quad_encode_msg/src/quad_encode_msg_nodelet.cpp
@@ -2,6 +2,7 @@
 #include <nodelet/nodelet.h>
 #include <quadrotor_msgs/SO3Command.h>
 #include <quadrotor_msgs/TRPYCommand.h>
+#include <quadrotor_msgs/PWMCommand.h>
 #include <quadrotor_msgs/encode_msgs.h>
 #include <quadrotor_msgs/Serial.h>
 
@@ -13,9 +14,11 @@ class QuadEncodeMsg : public nodelet::Nodelet
  private:
   void so3_cmd_callback(const quadrotor_msgs::SO3Command::ConstPtr &msg);
   void trpy_cmd_callback(const quadrotor_msgs::TRPYCommand::ConstPtr &msg);
+  void pwm_cmd_callback(const quadrotor_msgs::PWMCommand::ConstPtr &msg);
   ros::Publisher serial_msg_pub_;
   ros::Subscriber so3_cmd_sub_;
   ros::Subscriber trpy_cmd_sub_;
+  ros::Subscriber pwm_cmd_sub_;
   int channel_;
 
 };
@@ -46,6 +49,18 @@ void QuadEncodeMsg::trpy_cmd_callback(const quadrotor_msgs::TRPYCommand::ConstPt
   serial_msg_pub_.publish(serial_msg);
 }
 
+void QuadEncodeMsg::pwm_cmd_callback(const quadrotor_msgs::PWMCommand::ConstPtr &msg)
+{
+  quadrotor_msgs::Serial::Ptr serial_msg(new quadrotor_msgs::Serial);
+  serial_msg->header.seq = msg->header.seq;
+  serial_msg->channel = channel_;
+  serial_msg->type = quadrotor_msgs::Serial::PWM_CMD;
+  quadrotor_msgs::encodePWMCommand(*msg, serial_msg->data);
+
+  serial_msg->header.stamp = ros::Time::now();
+  serial_msg_pub_.publish(serial_msg);
+}
+
 void QuadEncodeMsg::onInit(void)
 {
   ros::NodeHandle priv_nh(getPrivateNodeHandle());
@@ -56,6 +71,8 @@ void QuadEncodeMsg::onInit(void)
                                                   ros::TransportHints().tcpNoDelay());
   trpy_cmd_sub_ = priv_nh.subscribe("trpy_cmd", 10, &QuadEncodeMsg::trpy_cmd_callback, this,
                                     ros::TransportHints().tcpNoDelay());
+  pwm_cmd_sub_ = priv_nh.subscribe("pwm_cmd", 10, &QuadEncodeMsg::pwm_cmd_callback, this,
+                                                  ros::TransportHints().tcpNoDelay());
 
   serial_msg_pub_ = priv_nh.advertise<quadrotor_msgs::Serial>("serial_msg", 10);
 }
