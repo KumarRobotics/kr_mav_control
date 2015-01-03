60b83a3d5e209c2b3eb75f146029b92c0aeb5721
Justin Thomas <justinthomas@jtwebs.net>
2015-01-02 19:52:27 -0500

    Changing angle corrections to signed values

--- asctec_firmware/sdk.h
+++ asctec_firmware/sdk.h
@@ -48,7 +48,7 @@ struct SO3_CMD_INPUT
   uint8_t kOm[3]; // /100
   int16_t cur_yaw; // /1e4
   int16_t kf_correction; // /1e11;
-  uint8_t angle_corrections[2]; // roll,pitch /2500
+  int8_t angle_corrections[2]; // roll,pitch /2500
   uint8_t enable_motors:1;
   uint8_t use_external_yaw:1;
   uint8_t use_angle_corrections:1;
