18adadf0dcff1f274d62ae10a12f4f264131cc67
Kartik Mohta <kartikmohta@gmail.com>
2015-01-05 23:57:00 -0500

    controllers_manager: Abstract classes should have a public virtual destructor

--- controllers/controllers_manager/include/controllers_manager/Controller.h
+++ controllers/controllers_manager/include/controllers_manager/Controller.h
@@ -7,17 +7,17 @@
 
 namespace controllers_manager
 {
-
 class Controller
 {
  public:
+  virtual ~Controller(void) {}
+
   virtual void Initialize(const ros::NodeHandle &nh) = 0;
   virtual bool Activate(void) = 0;
   virtual void Deactivate(void) = 0;
 
   virtual const quadrotor_msgs::PositionCommand::Ptr update(const nav_msgs::Odometry::ConstPtr &msg) = 0;
 };
-
 }
 
 #endif
