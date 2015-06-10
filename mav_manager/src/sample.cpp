47c2af173edc48912cdf279e13e07de910a0e33b
Justin Thomas <justinthomas@jtwebs.net>
2015-06-10 15:44:52 -0400

    Move srvs into the MAV_Services class

--- mav_manager/src/sample.cpp
+++ mav_manager/src/sample.cpp
@@ -3,19 +3,6 @@
 #include <mav_manager/Trigger.h>
 #include <mav_manager/Vec4.h>
 
-// Services
-ros::ServiceServer
-  srv_motors_,
-  srv_takeoff_,
-  srv_goHome_,
-  srv_goTo_,
-  srv_setDesVelWorld_,
-  srv_setDesVelBody_,
-  srv_useRadioForVelocity_,
-  srv_hover_,
-  srv_ehover_,
-  srv_eland_,
-  srv_estop_;
 
 // Typedefs
 typedef Eigen::Vector4d Vec4;
@@ -24,89 +11,124 @@ class MAV_Services
 {
   public:
 
-  // Let's make an MAV
-  MAVManager mav_;
+    // Services
+    ros::ServiceServer
+      srv_motors_,
+      srv_takeoff_,
+      srv_goHome_,
+      srv_goTo_,
+      srv_setDesVelWorld_,
+      srv_setDesVelBody_,
+      srv_useRadioForVelocity_,
+      srv_hover_,
+      srv_ehover_,
+      srv_eland_,
+      srv_estop_;
 
-  bool motors_cb(mav_manager::Bool::Request &req, mav_manager::Bool::Response &res)
-  {
-    mav_.motors(req.b);
-    res.success = true;
-    res.message = "Motors activated";
-    return true;
-  }
-  bool takeoff_cb(mav_manager::Trigger::Request &req, mav_manager::Trigger::Response &res)
-  {
-    res.success = mav_.takeoff();
-    res.message = "Takeoff";
-    return res.success;
-  }
-  bool goHome_cb(mav_manager::Trigger::Request &req, mav_manager::Trigger::Response &res)
-  {
-    res.success = mav_.goHome();
-    res.message = "Going home";
-    return res.success;
-  }
-  bool goTo_cb(mav_manager::Vec4::Request &req, mav_manager::Vec4::Response &res)
-  {
-    Vec4 goal(req.goal[0], req.goal[1], req.goal[2], req.goal[3]);
-    res.success = mav_.goTo(goal);
-    res.message = "Go To";
-    return res.success;
-  }
-  bool setDesVelWorld_cb(mav_manager::Vec4::Request &req, mav_manager::Vec4::Response &res)
-  {
-    Vec4 goal(req.goal[0], req.goal[1], req.goal[2], req.goal[3]);
-    res.success = mav_.setDesVelWorld(goal);
-    res.message = "World Velocity";
-    return res.success;
-  }
-  bool setDesVelBody_cb(mav_manager::Vec4::Request &req, mav_manager::Vec4::Response &res)
-  {
-    Vec4 goal(req.goal[0], req.goal[1], req.goal[2], req.goal[3]);
-    res.success = mav_.setDesVelBody(goal);
-    res.message = "Body Velocity";
-    return res.success;
-  }
-  bool useRadioForVelocity_cb(mav_manager::Bool::Request &req, mav_manager::Bool::Response &res)
-  {
-    res.success = mav_.useRadioForVelocity(req.b);
-    if (res.success)
+    // Let's make an MAV
+    MAVManager mav_;
+
+    bool motors_cb(mav_manager::Bool::Request &req, mav_manager::Bool::Response &res)
     {
-      if (req.b)
-        res.message = "Using radio for velocity";
-      else
-        res.message = "No longer using radio for velocity";
+      mav_.motors(req.b);
+      res.success = true;
+      res.message = "Motors activated";
+      return true;
     }
-    else
-      res.message = "Failed to transition";
+    bool takeoff_cb(mav_manager::Trigger::Request &req, mav_manager::Trigger::Response &res)
+    {
+      res.success = mav_.takeoff();
+      res.message = "Takeoff";
+      return res.success;
+    }
+    bool goHome_cb(mav_manager::Trigger::Request &req, mav_manager::Trigger::Response &res)
+    {
+      res.success = mav_.goHome();
+      res.message = "Going home";
+      return res.success;
+    }
+    bool goTo_cb(mav_manager::Vec4::Request &req, mav_manager::Vec4::Response &res)
+    {
+      Vec4 goal(req.goal[0], req.goal[1], req.goal[2], req.goal[3]);
+      res.success = mav_.goTo(goal);
+      res.message = "Go To";
+      return res.success;
+    }
+    bool setDesVelWorld_cb(mav_manager::Vec4::Request &req, mav_manager::Vec4::Response &res)
+    {
+      Vec4 goal(req.goal[0], req.goal[1], req.goal[2], req.goal[3]);
+      res.success = mav_.setDesVelWorld(goal);
+      res.message = "World Velocity";
+      return res.success;
+    }
+    bool setDesVelBody_cb(mav_manager::Vec4::Request &req, mav_manager::Vec4::Response &res)
+    {
+      Vec4 goal(req.goal[0], req.goal[1], req.goal[2], req.goal[3]);
+      res.success = mav_.setDesVelBody(goal);
+      res.message = "Body Velocity";
+      return res.success;
+    }
+    bool useRadioForVelocity_cb(mav_manager::Bool::Request &req, mav_manager::Bool::Response &res)
+    {
+      res.success = mav_.useRadioForVelocity(req.b);
+      if (res.success)
+      {
+        if (req.b)
+          res.message = "Using radio for velocity";
+        else
+          res.message = "No longer using radio for velocity";
+      }
+      else
+        res.message = "Failed to transition";
 
-    return res.success;
-  }
-  bool hover_cb(mav_manager::Trigger::Request &req, mav_manager::Trigger::Response &res)
-  {
-    res.success = mav_.hover();
-    res.message = "Hover";
-    return res.success;
-  }
-  bool ehover_cb(mav_manager::Trigger::Request &req, mav_manager::Trigger::Response &res)
-  {
-    res.success = mav_.ehover();
-    res.message = "Emergency Hover";
-    return res.success;
-  }
-  bool eland_cb(mav_manager::Trigger::Request &req, mav_manager::Trigger::Response &res)
-  {
-    res.success = mav_.eland();
-    res.message = "Emergency Landing";
-    return res.success;
-  }
-  bool estop_cb(mav_manager::Trigger::Request &req, mav_manager::Trigger::Response &res)
-  {
-    mav_.estop();
-    res.success = true;
-    res.message = "Emergency Stop";
-    return true;
-  }
+      return res.success;
+    }
+    bool hover_cb(mav_manager::Trigger::Request &req, mav_manager::Trigger::Response &res)
+    {
+      res.success = mav_.hover();
+      res.message = "Hover";
+      return res.success;
+    }
+    bool ehover_cb(mav_manager::Trigger::Request &req, mav_manager::Trigger::Response &res)
+    {
+      res.success = mav_.ehover();
+      res.message = "Emergency Hover";
+      return res.success;
+    }
+    bool eland_cb(mav_manager::Trigger::Request &req, mav_manager::Trigger::Response &res)
+    {
+      res.success = mav_.eland();
+      res.message = "Emergency Landing";
+      return res.success;
+    }
+    bool estop_cb(mav_manager::Trigger::Request &req, mav_manager::Trigger::Response &res)
+    {
+      mav_.estop();
+      res.success = true;
+      res.message = "Emergency Stop";
+      return true;
+    }
+
+    // Constructor
+    MAV_Services() : nh_(""), nh_priv_("~") {
+
+      // Services
+      srv_motors_ = nh_.advertiseService("motors", &MAV_Services::motors_cb, this);
+      srv_takeoff_ = nh_.advertiseService("takeoff", &MAV_Services::takeoff_cb, this);
+      srv_goHome_ = nh_.advertiseService("goHome", &MAV_Services::goHome_cb, this);
+      srv_goTo_ = nh_.advertiseService("goTo", &MAV_Services::goTo_cb, this);
+      srv_setDesVelWorld_ = nh_.advertiseService("setDesVelWorld", &MAV_Services::setDesVelWorld_cb, this);
+      srv_setDesVelBody_ = nh_.advertiseService("setDesVelBody", &MAV_Services::setDesVelBody_cb, this);
+      srv_useRadioForVelocity_ = nh_.advertiseService("useRadioForVelocity", &MAV_Services::useRadioForVelocity_cb, this);
+      srv_hover_ = nh_.advertiseService("hover", &MAV_Services::hover_cb, this);
+      srv_ehover_ = nh_.advertiseService("ehover", &MAV_Services::ehover_cb, this);
+      srv_eland_ = nh_.advertiseService("eland", &MAV_Services::eland_cb, this);
+      srv_estop_ = nh_.advertiseService("estop", &MAV_Services::estop_cb, this);
+    }
+
+  private:
+    ros::NodeHandle nh_;
+    ros::NodeHandle nh_priv_;
 };
 
 int main(int argc, char **argv)
@@ -115,19 +137,6 @@ int main(int argc, char **argv)
   ros::NodeHandle nh("");
 
   MAV_Services mav_srvs;
-
-  // Services
-  srv_motors_ = nh.advertiseService("motors", &MAV_Services::motors_cb, &mav_srvs);
-  srv_takeoff_ = nh.advertiseService("takeoff", &MAV_Services::takeoff_cb, &mav_srvs);
-  srv_goHome_ = nh.advertiseService("goHome", &MAV_Services::goHome_cb, &mav_srvs);
-  srv_goTo_ = nh.advertiseService("goTo", &MAV_Services::goTo_cb, &mav_srvs);
-  srv_setDesVelWorld_ = nh.advertiseService("setDesVelWorld", &MAV_Services::setDesVelWorld_cb, &mav_srvs);
-  srv_setDesVelBody_ = nh.advertiseService("setDesVelBody", &MAV_Services::setDesVelBody_cb, &mav_srvs);
-  srv_useRadioForVelocity_ = nh.advertiseService("useRadioForVelocity", &MAV_Services::useRadioForVelocity_cb, &mav_srvs);
-  srv_hover_ = nh.advertiseService("hover", &MAV_Services::hover_cb, &mav_srvs);
-  srv_ehover_ = nh.advertiseService("ehover", &MAV_Services::ehover_cb, &mav_srvs);
-  srv_eland_ = nh.advertiseService("eland", &MAV_Services::eland_cb, &mav_srvs);
-  srv_estop_ = nh.advertiseService("estop", &MAV_Services::estop_cb, &mav_srvs);
 
   // Let's spin some rotors
   ros::spin();
