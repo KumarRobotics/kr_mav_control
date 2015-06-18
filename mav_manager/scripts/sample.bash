fc10eeb277e283ae30efb809af13121e44d1833e
Justin Thomas <justinthomas@jtwebs.net>
2015-06-18 11:00:06 -0400

    Adds a sample bash script

---
+++ mav_manager/scripts/sample.bash
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+ROBOT="quadrotor"
+
+echo "Enable motors..."
+rosservice call /$ROBOT/motors true
+sleep 1
+
+read -p "Press [Enter] to takeoff"
+echo "Takeoff..."
+rosservice call /$ROBOT/takeoff
+sleep 1
+
+read -p "Press [Enter] to go to [1, 1, 1]"
+rosservice call /$ROBOT/goTo "goal: [1.0, 1.0, 1.0, 0.0]"
+sleep 1
+
+read -p "Press [Enter] to hover"
+rosservice call /$ROBOT/hover
+sleep 1
+
+read -p "Press [Enter] to go home"
+rosservice call /$ROBOT/goHome
+sleep 1
+
+read -p "Press [Enter] to estop"
+rosservice call /$ROBOT/estop
