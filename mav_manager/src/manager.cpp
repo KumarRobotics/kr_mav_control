6437a7d2468945e5bddb27d93e80e1d12ff9c5d1
Justin Thomas <justinthomas@jtwebs.net>
2015-07-27 12:32:49 -0400

    Forgot a block of code for the last commit

--- mav_manager/src/manager.cpp
+++ mav_manager/src/manager.cpp
@@ -352,21 +352,6 @@ void MAVManager::heartbeat() {
     this->eland();
   }
 
-  // Convert quaterions to tf so we can compute Euler angles, etc.
-  tf::Quaternion imu_q(imu_q_.x(), imu_q_.y(), imu_q_.z(), imu_q_.w());
-  tf::Quaternion odom_q(odom_q_.x(), odom_q_.y(), odom_q_.z(), odom_q_.w());
-
-  // Determine a geodesic angle from hover at the same yaw
-  double yaw, pitch, roll;
-  tf::Matrix3x3 R;
-
-  // If we don't have IMU feedback, imu_q_ will be the identity rotation
-  tf::Matrix3x3(imu_q).getEulerYPR(yaw, pitch, roll);
-  R.setEulerYPR(0, pitch, roll);
-  float imu_geodesic = std::fabs( std::acos(0.5 * (R[0][0] + R[1][1] + R[2][2] - 1)));
-
-  tf::Matrix3x3(odom_q).getEulerYPR(yaw, pitch, roll);
-  R.setEulerYPR(0, pitch, roll);
   // Checking for imu
   if (motors_ && need_imu_ && !this->have_recent_imu()) {
     ROS_WARN("No recent imu!");
