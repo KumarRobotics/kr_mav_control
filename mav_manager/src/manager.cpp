3cf49f3daf70169b98c6e59718bfb602264cd86c
Justin Thomas <justinthomas@jtwebs.net>
2015-09-08 22:13:29 -0400

    Add odom_q_ to MAV_Manager initializer list

--- mav_manager/src/manager.cpp
+++ mav_manager/src/manager.cpp
@@ -32,6 +32,7 @@ MAVManager::MAVManager()
       last_heartbeat_t_(0.0),
       mass_(-1.0),
       kGravity_(9.81),
+      odom_q_(1.0, 0.0, 0.0, 0.0),
       imu_q_(1.0, 0.0 ,0.0 ,0.0),
       max_attitude_angle_(45.0 / 180.0 * M_PI),
       need_imu_(false),
