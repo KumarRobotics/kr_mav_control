bdc0e2e826dd806b7c3a34aa80b1eccfd5e29cf1
Mike <wami@seas.upenn.edu>
2015-10-05 10:50:02 -0400

    fixed takeoff script with new trackers

--- quadrotor_simulator/launch/takeoff.sh
+++ quadrotor_simulator/launch/takeoff.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 rostopic pub -1 /quadrotor/motors std_msgs/Bool 0
-rostopic pub -1 /quadrotor/controllers_manager/line_tracker_min_jerk/goal geometry_msgs/Vector3 "{x: 0.0, y: 0.0, z: 2.0}"
-rosservice call /quadrotor/controllers_manager/transition line_tracker/LineTrackerMinJerk
+rostopic pub -1 /quadrotor/trackers_manager/line_tracker_min_jerk/goal quadrotor_msgs/LineTrackerGoal "{x: 0.5, y: 0.5, z: 2.0, yaw: 0.0, v_des: 0.0, a_des: 0.0}"
+rosservice call /quadrotor/trackers_manager/transition std_trackers/LineTrackerMinJerk
 rostopic pub -1 /quadrotor/motors std_msgs/Bool 1
