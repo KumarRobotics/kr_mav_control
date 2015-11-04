8de16d89f3f6eecdeff16ab1f9f071453849c308
Justin Thomas <justinthomas@jtwebs.net>
2015-11-04 11:18:43 -0500

    setDesVelIn* defaults to use_position_feedback = false

--- mav_manager/include/mav_manager/manager.h
+++ mav_manager/include/mav_manager/manager.h
@@ -61,8 +61,8 @@ class MAVManager
     bool goTo(Vec3 xyz, float yaw, Vec2 v_and_a_des = Vec2::Zero());
     bool goTo(Vec3 xyz, Vec2 v_and_a_des = Vec2::Zero());  // Uses Current yaw
 
-    bool setDesVelInWorldFrame(float x, float y, float z, float yaw, bool use_position_feedback = true);
-    bool setDesVelInBodyFrame(float x, float y, float z, float yaw, bool use_position_feedback = true);
+    bool setDesVelInWorldFrame(float x, float y, float z, float yaw, bool use_position_feedback = false);
+    bool setDesVelInBodyFrame(float x, float y, float z, float yaw, bool use_position_feedback = false);
 
     // Yaw control
     bool goToYaw(float);
