362a8531bdad2a281effa303bd3f96cb09e3f2cf
Giuseppe Loianno <loiannog@gmail.com>
2016-08-26 20:58:55 -0400

    adding corrections

--- qualcomm_interface/src/so3cmd_to_qualcomm_nodelet.cpp
+++ qualcomm_interface/src/so3cmd_to_qualcomm_nodelet.cpp
@@ -18,6 +18,7 @@ class SO3CmdToQualcomm : public nodelet::Nodelet
   void so3_cmd_callback(const quadrotor_msgs::SO3Command::ConstPtr &msg);
   void odom_callback(const nav_msgs::Odometry::ConstPtr &odom);
   void imu_callback(const sensor_msgs::Imu::ConstPtr &pose);
+  void so3_cmd_to_qc_interface(const quadrotor_msgs::SO3Command::ConstPtr &msg);
   void motors_on();
   void motors_off();
 
@@ -49,6 +50,16 @@ void SO3CmdToQualcomm::odom_callback(const nav_msgs::Odometry::ConstPtr &odom)
   odom_q_ = Eigen::Quaterniond(
       odom->pose.pose.orientation.w, odom->pose.pose.orientation.x,
       odom->pose.pose.orientation.y, odom->pose.pose.orientation.z);
+    if(so3_cmd_set_ &&
+     ((ros::Time::now() - last_so3_cmd_time_).toSec() >= so3_cmd_timeout_))
+  {
+    ROS_INFO("so3_cmd timeout. %f seconds since last command",
+             (ros::Time::now() - last_so3_cmd_time_).toSec());
+    const auto last_so3_cmd_ptr =
+        boost::make_shared<quadrotor_msgs::SO3Command>(last_so3_cmd_);
+
+    so3_cmd_callback(last_so3_cmd_ptr);
+  }
 }
 
 void SO3CmdToQualcomm::imu_callback(const sensor_msgs::Imu::ConstPtr &pose)
@@ -135,18 +146,8 @@ void SO3CmdToQualcomm::motors_off(){
 
 }
 
-void SO3CmdToQualcomm::so3_cmd_callback(
-    const quadrotor_msgs::SO3Command::ConstPtr &msg)
-{
-  if(!so3_cmd_set_)
-    so3_cmd_set_ = true;
-
-//switch on motors
-  if(msg->aux.enable_motors && !motor_status)
-  	motors_on();
-  else if(!msg->aux.enable_motors)
-  	motors_off();
-
+void SO3CmdToQualcomm::so3_cmd_to_qc_interface(
+    const quadrotor_msgs::SO3Command::ConstPtr &msg){
   // grab desired forces and rotation from so3
   const Eigen::Vector3d f_des(msg->force.x, msg->force.y, msg->force.z);
 
@@ -160,29 +161,7 @@ void SO3CmdToQualcomm::so3_cmd_callback(
   tf::Quaternion odom_tf =
       tf::Quaternion(odom_q_.x(), odom_q_.y(), odom_q_.z(), odom_q_.w());
 
-
-  // extract RPY's
-  //double imu_roll, imu_pitch, imu_yaw;
-  //double odom_roll, odom_pitch, odom_yaw;
-  //tf::Matrix3x3(imu_tf).getRPY(imu_roll, imu_pitch, imu_yaw);
-  //tf::Matrix3x3(odom_tf).getRPY(odom_roll, odom_pitch, odom_yaw);
-
-  // create only yaw tf:Quaternions
-  //tf::Quaternion imu_tf_yaw;
-  //tf::Quaternion odom_tf_yaw;
-  //imu_tf_yaw.setRPY(0.0, 0.0, imu_yaw);
-  //odom_tf_yaw.setRPY(0.0, 0.0, odom_yaw);
-  //const tf::Quaternion tf_imu_odom_yaw = imu_tf_yaw * odom_tf_yaw.inverse();
-
-  // transform!
-  //Eigen::Quaterniond q_des_transformed =
-  //    Eigen::Quaterniond(tf_imu_odom_yaw.w(), tf_imu_odom_yaw.x(),
-   //                      tf_imu_odom_yaw.y(), tf_imu_odom_yaw.z()) *
-   //   q_des;
-
-  // check psi for stability
-  //const Eigen::Matrix3d R_des(q_des_transformed);
-  const Eigen::Matrix3d R_cur(odom_q_);
+        const Eigen::Matrix3d R_cur(odom_q_);
 
   //const float Psi =
   //    0.5f * (3.0f - (R_des(0, 0) * R_cur(0, 0) + R_des(1, 0) * R_cur(1, 0) +
@@ -198,44 +177,8 @@ void SO3CmdToQualcomm::so3_cmd_callback(
                f_des(2) * R_cur(2, 2);
 
   //convert throttle in grams
-  throttle = throttle*1000/9.81;             
-  //}
-  //else
-  //{
-    //ROS_WARN_THROTTLE(1,"psi > 1.0, throttle set to 0.0 in mavros_interface.");
-  //}
+  throttle = throttle*1000/9.81; 
 
-  // Scale force to be proportional to rotor velocity (rad/s).
-  // Note: This ignores the number of propellers.
-  //throttle = std::sqrt(throttle / kf_);
-
-  // Scaling from proportional rotor velocity (rad/s) to att_throttle for pixhawk
-  //throttle = lin_cof_a_ * throttle + lin_int_b_;
-
-  // clamp from 0.0 to 1.0
-  //throttle = std::min(1.0, throttle);
-  //throttle = std::max(0.0, throttle);
-
-  //if(!msg->aux.enable_motors)
-    //throttle = 0;
-
-
-  // publish messages
-  //auto setpoint_msg = boost::make_shared<mavros_msgs::AttitudeTarget>();
-  //setpoint_msg->header = msg->header;
-  //setpoint_msg->type_mask = 0;
-  /*setpoint_msg->orientation.w = q_des_transformed.w();
-  setpoint_msg->orientation.x = q_des_transformed.x();
-  setpoint_msg->orientation.y = q_des_transformed.y();
-  setpoint_msg->orientation.z = q_des_transformed.z();
-  setpoint_msg->body_rate.x = msg->angular_velocity.x;
-  setpoint_msg->body_rate.y = msg->angular_velocity.y;
-  setpoint_msg->body_rate.z = msg->angular_velocity.z;
-  setpoint_msg->thrust = throttle;*/
-
-  //attitude_raw_pub_.publish(setpoint_msg);
-  //send attitude to the Qualcomm robot
-  //call the API to update the internal cache of flight 0 success
   int res_update = sn_update_data();
   if(res_update ==  -1){
   printf("\nTRAJ 1: flight software non functional\n");
@@ -246,7 +189,22 @@ void SO3CmdToQualcomm::so3_cmd_callback(
   int r = sn_send_thrust_att_ang_vel_command (throttle, q_des.w(), q_des.x(), q_des.y(), q_des.z(), ang_vel(0), ang_vel(1), ang_vel(2));
   if(r == -1)
   printf("\nTRAJ 2: control command not send\n");
+}
 
+void SO3CmdToQualcomm::so3_cmd_callback(
+    const quadrotor_msgs::SO3Command::ConstPtr &msg)
+{
+  if(!so3_cmd_set_)
+    so3_cmd_set_ = true;
+
+//switch on motors
+  if(msg->aux.enable_motors && !motor_status)
+  	motors_on();
+  else if(!msg->aux.enable_motors)
+  	motors_off();
+
+  so3_cmd_to_qc_interface(msg);
+  
   // save last so3_cmd
   last_so3_cmd_ = *msg;
   last_so3_cmd_time_ = ros::Time::now();
