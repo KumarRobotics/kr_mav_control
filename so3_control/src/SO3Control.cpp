bc84377e8e13da06903f812e80b3a4df0cfdbcb3
Kartik Mohta <kartikmohta@gmail.com>
2016-11-26 19:18:46 -0500

    Undo the formatting changes in SO3Control.cpp

--- so3_control/src/SO3Control.cpp
+++ so3_control/src/SO3Control.cpp
@@ -1,6 +1,10 @@
 #include "so3_control/SO3Control.h"
 
-SO3Control::SO3Control() : mass_(0.5), g_(9.81), max_pos_int_(0.5), cos_max_tilt_angle_(-1.0)
+SO3Control::SO3Control()
+  : mass_(0.5),
+    g_(9.81),
+    max_pos_int_(0.5),
+    cos_max_tilt_angle_(-1.0)
 {
 }
 
@@ -35,9 +39,14 @@ void SO3Control::setMaxTiltAngle(const float max_tilt_angle)
     cos_max_tilt_angle_ = std::cos(max_tilt_angle);
 }
 
-void SO3Control::calculateControl(const Eigen::Vector3f &des_pos, const Eigen::Vector3f &des_vel,
-                                  const Eigen::Vector3f &des_acc, const Eigen::Vector3f &des_jerk, const float des_yaw,
-                                  const float des_yaw_dot, const Eigen::Vector3f &kx, const Eigen::Vector3f &kv,
+void SO3Control::calculateControl(const Eigen::Vector3f &des_pos,
+                                  const Eigen::Vector3f &des_vel,
+                                  const Eigen::Vector3f &des_acc,
+                                  const Eigen::Vector3f &des_jerk,
+                                  const float des_yaw,
+                                  const float des_yaw_dot,
+                                  const Eigen::Vector3f &kx,
+                                  const Eigen::Vector3f &kv,
                                   const Eigen::Vector3f &ki)
 {
   const Eigen::Vector3f e_pos = des_pos - pos_;
@@ -46,7 +55,7 @@ void SO3Control::calculateControl(const Eigen::Vector3f &des_pos, const Eigen::V
   for(int i = 0; i < 3; i++)
   {
     if(kx(i) != 0)
-      pos_int_(i) += ki(i) * e_pos(i);
+      pos_int_(i) += ki(i)*e_pos(i);
 
     // Limit integral term
     if(pos_int_(i) > max_pos_int_)
@@ -55,7 +64,7 @@ void SO3Control::calculateControl(const Eigen::Vector3f &des_pos, const Eigen::V
       pos_int_(i) = -max_pos_int_;
   }
 
-  // std::cout << "pos_int: " << pos_int_.transpose() << std::endl;
+  //std::cout << "pos_int: " << pos_int_.transpose() << std::endl;
 
   const Eigen::Vector3f acc_grav = g_ * Eigen::Vector3f::UnitZ();
   const Eigen::Vector3f acc_control = kx.asDiagonal() * e_pos + kv.asDiagonal() * e_vel + pos_int_ + des_acc;
@@ -74,7 +83,7 @@ void SO3Control::calculateControl(const Eigen::Vector3f &des_pos, const Eigen::V
 
   force_.noalias() = mass_ * acc_total;
 
-  // std::cout << "Force: " << force_.transpose() << std::endl;
+  //std::cout << "Force: " << force_.transpose() << std::endl;
 
   Eigen::Vector3f b1c, b2c, b3c;
   const Eigen::Vector3f b2d(-std::sin(des_yaw), std::cos(des_yaw), 0);
@@ -87,12 +96,10 @@ void SO3Control::calculateControl(const Eigen::Vector3f &des_pos, const Eigen::V
   b1c.noalias() = b2d.cross(b3c).normalized();
   b2c.noalias() = b3c.cross(b1c).normalized();
 
-  const Eigen::Vector3f force_dot =
-      mass_ * lambda * (kx.asDiagonal() * e_vel + des_jerk); // Ignoring kv*e_acc and ki*e_pos terms
-  const Eigen::Vector3f b3c_dot = b3c.cross(force_dot / force_.norm()).cross(b3c);
-  const Eigen::Vector3f b2d_dot(-std::cos(des_yaw) * des_yaw_dot, -std::sin(des_yaw) * des_yaw_dot, 0);
-  const Eigen::Vector3f b1c_dot =
-      b1c.cross(((b2d_dot.cross(b3c) + b2d.cross(b3c_dot)) / (b2d.cross(b3c)).norm()).cross(b1c));
+  const Eigen::Vector3f force_dot = mass_*lambda*(kx.asDiagonal()*e_vel + des_jerk); // Ignoring kv*e_acc and ki*e_pos terms
+  const Eigen::Vector3f b3c_dot = b3c.cross(force_dot/force_.norm()).cross(b3c);
+  const Eigen::Vector3f b2d_dot(-std::cos(des_yaw)*des_yaw_dot, -std::sin(des_yaw)*des_yaw_dot, 0);
+  const Eigen::Vector3f b1c_dot = b1c.cross(((b2d_dot.cross(b3c)+b2d.cross(b3c_dot))/(b2d.cross(b3c)).norm()).cross(b1c));
   const Eigen::Vector3f b2c_dot = b3c_dot.cross(b1c) + b3c.cross(b1c_dot);
 
   Eigen::Matrix3f R;
@@ -102,8 +109,8 @@ void SO3Control::calculateControl(const Eigen::Vector3f &des_pos, const Eigen::V
   Eigen::Matrix3f R_dot;
   R_dot << b1c_dot, b2c_dot, b3c_dot;
 
-  const Eigen::Matrix3f omega_hat = R.transpose() * R_dot;
-  angular_velocity_ = Eigen::Vector3f(omega_hat(2, 1), omega_hat(0, 2), omega_hat(1, 0));
+  const Eigen::Matrix3f omega_hat = R.transpose()*R_dot;
+  angular_velocity_ = Eigen::Vector3f(omega_hat(2,1), omega_hat(0,2), omega_hat(1,0));
 }
 
 const Eigen::Vector3f &SO3Control::getComputedForce(void)
