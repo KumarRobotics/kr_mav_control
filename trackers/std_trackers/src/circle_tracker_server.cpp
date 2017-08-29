d4e32457865e0bd0089d1cc8c318ddafe45724fd
sytang <sarahyifang@gmail.com>
2017-08-29 13:42:28 -0400

    Parameterized circle tracker

--- trackers/std_trackers/src/circle_tracker_server.cpp
+++ trackers/std_trackers/src/circle_tracker_server.cpp
@@ -9,6 +9,7 @@
 #include <ros/ros.h>
 #include <tf/transform_datatypes.h>
 #include <actionlib/server/simple_action_server.h>
+#include <std_msgs/Empty.h>
 
 #include <trackers_manager/Tracker.h>
 #include <quadrotor_msgs/PositionCommand.h>
@@ -83,6 +84,9 @@ private:
   float current_yaw_;
   float constant_yaw_;
 
+  // Pubish trigger for start and goal.
+  ros::Publisher pub_start_;
+  ros::Publisher pub_end_;
 };
 
 CircleTrackerAction::CircleTrackerAction(void) :
@@ -104,7 +108,7 @@ void CircleTrackerAction::Initialize(const ros::NodeHandle &nh) {
   nh.param("gains/vel/x", kv_[0], 2.2);
   nh.param("gains/vel/y", kv_[1], 2.2);
   nh.param("gains/vel/z", kv_[2], 4.0);
-  nh.param("alpha_des", alpha_des_, static_cast<float>(M_PI / 10.0));
+  nh.param("alpha_des", alpha_des_, static_cast<float>(M_PI / 20.0));
 
   ros::NodeHandle priv_nh(nh, "circle_tracker");
 
@@ -114,6 +118,9 @@ void CircleTrackerAction::Initialize(const ros::NodeHandle &nh) {
   tracker_server_->registerPreemptCallback(boost::bind(&CircleTrackerAction::preempt_callback, this));
 
   tracker_server_->start();
+
+  pub_start_ = priv_nh.advertise<std_msgs::Empty>("traj_start", 10);
+  pub_end_ = priv_nh.advertise<std_msgs::Empty>("traj_end", 10);
 }
 
 bool CircleTrackerAction::Activate(const quadrotor_msgs::PositionCommand::ConstPtr &cmd) {
@@ -143,7 +150,7 @@ bool CircleTrackerAction::Activate(const quadrotor_msgs::PositionCommand::ConstP
   // Calculate the offset needed so that
   // x(0) + offset = current_pos_.
   // Here, x(0) is [Ax 0 0].
-  offset_pos_ = current_pos_ - Eigen::Vector3f(Ax_, Ay_, 0.0);
+  offset_pos_ = current_pos_ - Eigen::Vector3f(Ax_, 0.0, 0.0);
   constant_yaw_ = current_yaw_;
   //   std::cout << " RELATIVE ACTIVATE CALCULATED AS " << offset_pos_(0) << " " << offset_pos_(1) << " " << offset_pos_(2) << " " << offset_yaw_ << "\n";
 
@@ -210,7 +217,8 @@ const quadrotor_msgs::PositionCommand::ConstPtr CircleTrackerAction::update(cons
     cmd->position.x = final_pos_(0);
     cmd->position.y = final_pos_(1);
     cmd->position.z = final_pos_(2);
-    cmd->yaw = current_yaw_;
+
+    cmd->yaw = constant_yaw_;
 
     cmd->velocity.x = 0.0;
     cmd->velocity.y = 0.0;
@@ -221,73 +229,113 @@ const quadrotor_msgs::PositionCommand::ConstPtr CircleTrackerAction::update(cons
     cmd->jerk.x = 0.0;
     cmd->jerk.y = 0.0;
     cmd->jerk.z = 0.0;
+
     cmd->yaw_dot = 0.0;
   }
   else {
     float theta, theta_dot, theta_ddot, theta_dddot;
 
-    theta = omega_des_ * traj_time;
-    theta_dot = 0.0;
-    theta_ddot = 0.0;
-    theta_dddot = 0.0;
+    // theta = omega_des_ * traj_time;
+    // theta_dot = omega_des_;
+    // theta_ddot = 0.0;
+    // theta_dddot = 0.0;
+    // theta_d4dot = 0.0;
+    // theta_d5dot = 0.0;
+    // theta_d6dot = 0.0;
 
-    // // Ramping up the angular velocity.
-    // if (traj_time > ramp_time_) {
-    //   /*      theta = 0.5 * alpha_des_ * traj_time_ * traj_time_;
-    //         theta_dot = alpha_des_ * traj_time_;
-    //         theta_ddot = alpha_des_;
-    //         theta_dddot = 0.0;*/
-    //   const float dT = traj_time;
-    //   const float dT2 = dT * dT, dT3 = dT2 * dT, dT4 = dT3 * dT, dT5 = dT4 * dT, dT6 = dT5 * dT;
-    //   theta = omega_coeffs_.at(0) * dT + 0.5 * omega_coeffs_.at(1) * dT2 + 1.0 / 3.0 * omega_coeffs_.at(2) * dT3 + 0.25 * omega_coeffs_.at(3) * dT4 + 1.0 / 5.0 * omega_coeffs_.at(4) * dT5 + 1.0 / 6.0 * omega_coeffs_.at(5) * dT6;
-    //   theta_dot = omega_coeffs_.at(0) + omega_coeffs_.at(1) * dT + omega_coeffs_.at(2) * dT2 + omega_coeffs_.at(3) * dT3 + omega_coeffs_.at(4) * dT4 + omega_coeffs_.at(5) * dT5;
-    //   theta_ddot = omega_coeffs_.at(1) + 2.0 * omega_coeffs_.at(2) * dT + 3.0 * omega_coeffs_.at(3) * dT2 + 4.0 * omega_coeffs_.at(4) * dT3 + 5.0 * omega_coeffs_.at(5) * dT4;
-    //   theta_dddot = 2.0 * omega_coeffs_.at(2) + 6.0 * omega_coeffs_.at(3) * dT + 12.0 * omega_coeffs_.at(3) * dT2 + 20.0 * omega_coeffs_.at(5) * dT3;
-    //   //theta_ddddot = 6.0*omega_coeffs_.at(3) + 24.0*omega_coeffs_.at(3)*dT + 60.0*omega_coeffs_.at(5)*dT2;
-    // }
-    // // Constant segment.
-    // else if (traj_time < circle_time_) {
-    //   // Calculate time in this phase.
-    //   const float dT = traj_time - ramp_time_;
+    // Ramping up the angular velocity.
+    if (traj_time < ramp_time_) {
+      //ROS_INFO("Circle Tracker: Accelerating...");
 
-    //   theta = omega_des_ * dT + ramp_dist_;
-    //   theta_dot = omega_des_;
-    //   theta_ddot = 0.0;
-    //   theta_dddot = 0.0;
-    // }
-    // // Ramping down the angular velocity.
-    // else {
-    //   // const float dT = traj_time - circle_time_;
-    //   // theta = -0.5 * alpha_des_ * dT * dT + omega_des_ * dT + circle_dist_;
-    //   // theta_dot = -alpha_des_ * dT + omega_des_;
-    //   // theta_ddot = -alpha_des_;
-    //   // theta_dddot = 0.0;
+      /*      theta = 0.5 * alpha_des_ * traj_time_ * traj_time_;
+            theta_dot = alpha_des_ * traj_time_;
+            theta_ddot = alpha_des_;
+            theta_dddot = 0.0;*/
+      const float dT = traj_time / ramp_time_;
+      //std::cout << " ramp time i s" << ramp_time_ << " and dt is " << dT << "\n";
+      const float dT2 = dT * dT, dT3 = dT2 * dT, dT4 = dT3 * dT, dT5 = dT4 * dT, dT6 = dT5 * dT;
+      theta = omega_coeffs_.at(0) * dT + 0.5 * omega_coeffs_.at(1) * dT2 + 1.0 / 3.0 * omega_coeffs_.at(2) * dT3 + 0.25 * omega_coeffs_.at(3) * dT4 + 1.0 / 5.0 * omega_coeffs_.at(4) * dT5 + 1.0 / 6.0 * omega_coeffs_.at(5) * dT6;
+      theta_dot = omega_coeffs_.at(0) + omega_coeffs_.at(1) * dT + omega_coeffs_.at(2) * dT2 + omega_coeffs_.at(3) * dT3 + omega_coeffs_.at(4) * dT4 + omega_coeffs_.at(5) * dT5;
+      theta_dot /= ramp_time_;
+      theta_ddot = omega_coeffs_.at(1) + 2.0 * omega_coeffs_.at(2) * dT + 3.0 * omega_coeffs_.at(3) * dT2 + 4.0 * omega_coeffs_.at(4) * dT3 + 5.0 * omega_coeffs_.at(5) * dT4;
+      const float ramp_time_2 = ramp_time_ * ramp_time_;
+      theta_ddot /= ramp_time_2;
+      theta_dddot = 2.0 * omega_coeffs_.at(2) + 6.0 * omega_coeffs_.at(3) * dT + 12.0 * omega_coeffs_.at(4) * dT2 + 20.0 * omega_coeffs_.at(5) * dT3;
+      const float ramp_time_3 = ramp_time_2 * ramp_time_;
+      theta_dddot /= (ramp_time_3);
+    }
+    // Constant segment.
+    else if (traj_time < circle_time_) {
+      //ROS_INFO("Circle Tracker: Coasting...");
+      std_msgs::Empty empty_msg;
+      pub_start_.publish(empty_msg);
 
-    //   const float dT = traj_time - circle_time_;
-    //   const float dT2 = dT * dT, dT3 = dT2 * dT, dT4 = dT3 * dT, dT5 = dT4 * dT, dT6 = dT5 * dT;
-    //   theta = circle_dist_ + omega_des_ * dT - (omega_coeffs_.at(0) * dT + 0.5 * omega_coeffs_.at(1) * dT2 + 1.0 / 3.0 * omega_coeffs_.at(2) * dT3 + 0.25 * omega_coeffs_.at(3) * dT4 + 1.0 / 5.0 * omega_coeffs_.at(4) * dT5 + 1.0 / 6.0 * omega_coeffs_.at(5) * dT6);
-    //   theta_dot = omega_des_ - (omega_coeffs_.at(0) + omega_coeffs_.at(1) * dT + omega_coeffs_.at(2) * dT2 + omega_coeffs_.at(3) * dT3 + omega_coeffs_.at(4) * dT4 + omega_coeffs_.at(5) * dT5);
-    //   theta_ddot = -(omega_coeffs_.at(1) + 2.0 * omega_coeffs_.at(2) * dT + 3.0 * omega_coeffs_.at(3) * dT2 + 4.0 * omega_coeffs_.at(4) * dT3 + 5.0 * omega_coeffs_.at(5) * dT4);
-    //   theta_dddot = -(2.0 * omega_coeffs_.at(2) + 6.0 * omega_coeffs_.at(3) * dT + 12.0 * omega_coeffs_.at(3) * dT2 + 20.0 * omega_coeffs_.at(5) * dT3);
+      // Calculate time in this phase.
+      const float dT = traj_time - ramp_time_;
 
-    // }
+      theta = omega_des_ * dT + ramp_dist_;
+      theta_dot = omega_des_;
+      theta_ddot = 0.0;
+      theta_dddot = 0.0;
 
-    const float pos_x = Ax_ * cos(theta);
-    const float pos_y = Ay_ * sin(theta);
+    }
+    // Ramping down the angular velocity.
+    else {
+      // const float dT = traj_time - circle_time_;
+      //       theta = omega_des_ * dT + circle_dist_;
+      // theta_dot = omega_des_;
+      // theta_ddot = 0.0;
+      // theta_dddot = 0.0;
+      // theta_d4dot = 0.0;
+      // theta_d5dot = 0.0;
+      // theta_d6dot = 0.0;
+
+      //ROS_INFO("Circle Tracker: Decelerating...");
+
+      //Publish message indicating trajectory end.
+      std_msgs::Empty empty_msg;
+      pub_end_.publish(empty_msg);
+
+      // const float dT = traj_time - circle_time_;
+      // theta = -0.5 * alpha_des_ * dT * dT + omega_des_ * dT + circle_dist_;
+      // theta_dot = -alpha_des_ * dT + omega_des_;
+      // theta_ddot = -alpha_des_;
+      // theta_dddot = 0.0;
+
+      const float dT = 1.0 - (traj_time - circle_time_) / ramp_time_;
+      const float dT2 = dT * dT, dT3 = dT2 * dT, dT4 = dT3 * dT, dT5 = dT4 * dT, dT6 = dT5 * dT;
+      theta = omega_coeffs_.at(0) * dT + 0.5 * omega_coeffs_.at(1) * dT2 + 1.0 / 3.0 * omega_coeffs_.at(2) * dT3 + 0.25 * omega_coeffs_.at(3) * dT4 + 1.0 / 5.0 * omega_coeffs_.at(4) * dT5 + 1.0 / 6.0 * omega_coeffs_.at(5) * dT6;
+      theta = circle_dist_ + ramp_dist_ - theta;
+      theta_dot = omega_coeffs_.at(0) + omega_coeffs_.at(1) * dT + omega_coeffs_.at(2) * dT2 + omega_coeffs_.at(3) * dT3 + omega_coeffs_.at(4) * dT4 + omega_coeffs_.at(5) * dT5;
+      theta_dot /= ramp_time_;
+      theta_ddot = omega_coeffs_.at(1) + 2.0 * omega_coeffs_.at(2) * dT + 3.0 * omega_coeffs_.at(3) * dT2 + 4.0 * omega_coeffs_.at(4) * dT3 + 5.0 * omega_coeffs_.at(5) * dT4;
+      const float ramp_time_2 = ramp_time_ * ramp_time_;
+      theta_ddot /= ramp_time_2;
+      theta_dddot = 2.0 * omega_coeffs_.at(2) + 6.0 * omega_coeffs_.at(3) * dT + 12.0 * omega_coeffs_.at(4) * dT2 + 20.0 * omega_coeffs_.at(5) * dT3;
+      const float ramp_time_3 = ramp_time_2 * ramp_time_;
+      theta_dddot /= (ramp_time_3);
+    }
+
+    const float sin_t = sin(theta);
+    const float cos_t = cos(theta);
+
+    const float pos_x = Ax_ * cos_t;
+    const float pos_y = Ay_ * sin_t;
     const float pos_z = 0.0;
 
-    const float vel_x = Ax_ * (-sin(theta) * theta_dot);
-    const float vel_y = Ay_ * (cos(theta) * theta_dot);
+    const float vel_x = Ax_ * (-sin_t * theta_dot);
+    const float vel_y = Ay_ * (cos_t * theta_dot);
     const float vel_z = 0.0;
 
-    const float acc_x = Ax_ * (-cos(theta) * theta_dot * theta_dot - sin(theta) * theta_ddot);
-    const float acc_y = Ay_ * (-sin(theta) * theta_dot * theta_dot + cos(theta) * theta_ddot);
+    const float theta_dot_2 = theta_dot * theta_dot;
+    const float acc_x = Ax_ * (-cos_t * theta_dot_2 - sin_t * theta_ddot);
+    const float acc_y = Ay_ * (-sin_t * theta_dot_2 + cos_t * theta_ddot);
     const float acc_z = 0.0;
 
-    const float jerk_x = Ax_ * (sin(theta) * theta_dot * theta_dot * theta_dot  - 3.0 * cos(theta) * theta_dot * theta_ddot - sin(theta) * theta_dddot);
-    const float jerk_y = Ay_ * (-cos(theta) * theta_dot * theta_dot * theta_dot - 3.0 * sin(theta) * theta_dot * theta_ddot + cos(theta) * theta_dddot);
+    const float theta_dot_3 = theta_dot_2 * theta_dot;
+    const float jerk_x = Ax_ * (sin_t * theta_dot_3  - 3.0 * cos_t * theta_dot * theta_ddot - sin_t * theta_dddot);
+    const float jerk_y = Ay_ * (-cos_t * theta_dot_3 - 3.0 * sin_t * theta_dot * theta_ddot + cos_t * theta_dddot);
     const float jerk_z = 0.0;
-
 
     // Note, these values do NOT yet include the offset.
     cmd->position.x = pos_x;
@@ -374,25 +422,34 @@ void CircleTrackerAction::goal_callback() {
   const float dT = omega_des_ / alpha_des_;
 
   Eigen::MatrixXf Ainv(6, 6);
-  Ainv << 1.0, 0.0, 0.0, 0.0, 0.0,
-       0.0, 0.0, 1.0, 0.0, 0.0, 0.0,
-       0.0, 0.0, 0.0, 0.0, 0.5, 0.0,
-       -10.0, 10.0, -6.0, -4.0, -1.5, 0.5,
-       15.0, -15.0, 8.0, 7.0, 1.5, -1.0,
-       -6.0, 6.0, -3.0, -3.0, -0.5, 0.5;
+  Ainv << 1.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+       0.0, 1.0, 0.0, 0.0, 0.0, 0.0,
+       0.0, 0.0, 0.5, 0.0, 0.0, 0.0,
+       -10.0, -6.0, -1.5, 10.0, -4.0, 0.5,
+       15.0, 8.0, 1.5, -15.0, 7.0, -1.0,
+       -6.0, -3.0, -0.5, 6.0, -3.0, 0.5;
   Eigen::VectorXf b(6);
-  b << 0.0, 0.0, 0.0, omega_des_, 0.0, 0.0;
+  b << 0.0, 0.0, 0.0, omega_des_ * dT, 0.0, 0.0;
+  //std::cout << "A_inv" << Ainv(3, 0) << " " << Ainv(3, 1) << " " << Ainv(3, 2) << " " << Ainv(3, 3) << " " << Ainv(3, 4) << " " << Ainv(3, 5) << " and b " << b(3) << "\n";
   Eigen::VectorXf omega_coeffs_vec = Ainv * b;
+ // std::cout << " rowwise multiply " << Ainv.row(3) * b << "\n";
   omega_coeffs_.resize(6);
+  // Track the angle traveled when ramping up.
+  // We have coefficients for omega(t).
+  // We need to evaluate theta(t) = int_0^t omega(t) at 1.
+  ramp_dist_ = 0.0;
   // Scale the trajectory back.
-  const std::vector<float> T_powers = {1, dT, dT * dT, dT*dT * dT, dT*dT*dT * dT, dT*dT*dT*dT * dT};
+  //const std::vector<float> T_powers = {1, dT, dT * dT, dT*dT * dT, dT*dT*dT * dT, dT*dT*dT*dT * dT};
   for (int ii = 0; ii < 6; ++ii) {
-    omega_coeffs_.at(ii) = omega_coeffs_vec(ii) / T_powers.at(ii);
+  //  std::cout << " the condition here is " << b(ii) << "\n";
+    omega_coeffs_.at(ii) = omega_coeffs_vec(ii); //omega_coeffs_vec(ii) / T_powers.at(ii);
+    //std::cout << " coefficient " << ii << " is " << omega_coeffs_vec(ii) << "\n";
+    ramp_dist_ += omega_coeffs_.at(ii) * 1.0 / static_cast<float>(ii + 1);
   }
 
   // Calculate the offset needed so that
   // x(0) + offset = current_pos_.
-  offset_pos_ = current_pos_ - Eigen::Vector3f(Ax_, Ay_, 0.0);
+  offset_pos_ = current_pos_ - Eigen::Vector3f(Ax_, 0.0, 0.0);
   //  std::cout << " RELATIVE CALCULATED AS " << offset_pos_(0) << " " << offset_pos_(1) << " " << offset_pos_(2) << " " << offset_yaw_ << "\n";
 
   ramp_time_ = dT;
@@ -400,8 +457,10 @@ void CircleTrackerAction::goal_callback() {
   traj_duration_ = 2.0 * ramp_time_ + msg->duration;
 
   // Calculate the distances needed.
-  ramp_dist_ = omega_coeffs_vec.sum();
   circle_dist_ = ramp_dist_ + omega_des_ * msg->duration;
+
+ // std::cout << " CIRCLE DESIGNED: ramp time " << ramp_time_ << " and circle time " << circle_time_ << " and total duration " << traj_duration_ << " the ramp dist " << ramp_dist_ * 180.0 / M_PI << " and circle dist " << circle_dist_ * 180.0 / M_PI << " to get to angular velocity " << omega_des_ << "\n";
+
 
   traj_started_ = false;
   traj_completed_ = false;
