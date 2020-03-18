6634989bb59239c2337da73d1995ac59a6b4d5ba
Dinesh Thakur <tdinesh@seas.upenn.edu>
2020-03-18 12:00:04 -0400

    add snav status publisher, fix rpm timestamp, loop rate

--- qualcomm_interface/src/status_publisher.cpp
+++ qualcomm_interface/src/status_publisher.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <ros/ros.h>
 #include <quadrotor_msgs/MotorRPM.h>
+#include <std_msgs/String.h>
+#include <std_msgs/Bool.h>
+#include <sensor_msgs/BatteryState.h>
+#include <sensor_msgs/Joy.h>
 #include <tf/transform_broadcaster.h>
 #include <snav/snapdragon_navigator.h>
 
@@ -8,10 +12,14 @@ int main(int argc, char *argv[])
 {
   ros::init(argc, argv, "motor_rpm_publisher");
   ros::NodeHandle nh;
-  ros::NodeHandle pnh("~");
   int rate;
-  pnh.param("rate", rate, 100);
-  ros::Publisher motor_speeds_pub_ = nh.advertise<quadrotor_msgs::MotorRPM>("motor_rpm", 2);
+  nh.param("rate", rate, 5);
+  ros::Publisher battery_pub_ = nh.advertise<sensor_msgs::BatteryState>("battery", 2);
+  ros::Publisher joy_pub = nh.advertise<sensor_msgs::Joy>("spektrum_joy", 2);
+
+  ros::Publisher props_state_pub = nh.advertise<std_msgs::String>("props_state", 2);
+  ros::Publisher on_ground_pub = nh.advertise<std_msgs::Bool>("on_ground", 2);
+
   ros::Rate loop_rate(rate);
 
   int update_ret;
@@ -75,15 +83,43 @@ int main(int argc, char *argv[])
       data_time.fromNSec(stamp*1e3);
       data_time += monotonic_offset;
 
-      quadrotor_msgs::MotorRPM speed;
-      int16_t *rpm = snav_cached_data_struct->esc_raw.rpm;
-      speed.header.stamp = data_time;
-      speed.motor_count = 4;
-      speed.rpm[0] = *rpm;
-      speed.rpm[1] = *(rpm+1);
-      speed.rpm[2] = *(rpm+2);
-      speed.rpm[3] = *(rpm+3);
-      motor_speeds_pub_.publish(speed);
+      //Battery
+      sensor_msgs::BatteryState bat_state;
+      bat_state.header.stamp = data_time;
+      bat_state.voltage = snav_cached_data_struct->general_status.voltage;
+      bat_state.current = -snav_cached_data_struct->general_status.current;
+      battery_pub_.publish(bat_state);
+
+      //Spektrum joy
+      /*
+      int32_t rc_status = snav_cached_data_struct->data_status.spektrum_rc_0_status;
+      uint8_t num_channels = snav_cached_data_struct->spektrum_rc_0_raw.num_channels;
+      uint8_t max_channels = 16;
+      sensor_msgs::Joy joy;
+      //joy.axes.resize();
+      //joy.buttons.resize();
+      */
+
+
+      //On Ground
+      bool on_ground = snav_cached_data_struct->general_status.on_ground;
+      on_ground_pub.publish(on_ground);
+
+      //Prop status
+      std_msgs::String props_state_msg;
+      if ((SnPropsState)snav_cached_data_struct->general_status.props_state == SN_PROPS_STATE_NOT_SPINNING) {
+        props_state_msg.data = "NOT_SPINNING";
+      }
+      else if ((SnPropsState)snav_cached_data_struct->general_status.props_state == SN_PROPS_STATE_STARTING) {
+        props_state_msg.data = "STARTING";
+      }
+      else if ((SnPropsState)snav_cached_data_struct->general_status.props_state == SN_PROPS_STATE_SPINNING) {
+        props_state_msg.data = "SPINNING";
+      }
+      else {
+        props_state_msg.data = "UNKNOWN";
+      }
+      props_state_pub.publish(props_state_msg);
 
       ros::spinOnce();
       loop_rate.sleep();
