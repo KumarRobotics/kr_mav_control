a112de4c710692147534227ae3c8f78928085fcd
Dinesh Thakur <tdinesh@seas.upenn.edu>
2020-03-22 02:53:22 -0400

    add time debugs

--- qualcomm_interface/src/rpm_publisher.cpp
+++ qualcomm_interface/src/rpm_publisher.cpp
@@ -46,13 +46,19 @@ int main(int argc, char *argv[])
   sntime.fromNSec(stmp*1000);
   ros::Duration snav_offset = realtime - sntime;
 
+  int64_t dt_timestamp_ns = (int64_t) snav_cached_data_struct->data_status.time * 1000;
+  ros::Time dttime;
+  dttime.fromNSec(dt_timestamp_ns);
+  ros::Duration time_offset = realtime - dttime;
+
   ROS_INFO_STREAM("monotonic time: " << monotonic.toSec());
-  ROS_INFO_STREAM("real time: " << realtime.toSec());
-  ROS_INFO_STREAM("snav time: " << sntime.toSec());
+  ROS_INFO_STREAM("real time: " << realtime);
+  ROS_INFO_STREAM("snav time: " << sntime);
+  ROS_INFO_STREAM("data time: " << dttime);
 
-  ROS_INFO_STREAM("Snav offset: " << snav_offset);
   ROS_INFO_STREAM("Monotonic offset: " << monotonic_offset);
-
+  ROS_INFO_STREAM("Snav offset: " << snav_offset);
+  ROS_INFO_STREAM("Data status offset: " << time_offset);
 
   while(ros::ok())
   {
@@ -73,6 +79,18 @@ int main(int argc, char *argv[])
       ros::Time data_time;
       data_time.fromNSec(stamp*1000);
       data_time += monotonic_offset;
+
+      ros::Time gen_time, dt_time, esc_time;
+      int64_t gen_timestamp_ns = (int64_t) snav_cached_data_struct->general_status.time * 1000;
+      gen_time.fromNSec(gen_timestamp_ns);
+
+      int64_t data_timestamp_ns = (int64_t) snav_cached_data_struct->data_status.time * 1000;
+      dt_time.fromNSec(data_timestamp_ns);
+
+      int64_t esc_timestamp_ns = (int64_t) snav_cached_data_struct->esc_raw.time * 1000;
+      esc_time.fromNSec(esc_timestamp_ns);
+
+      ROS_INFO_STREAM("Gen time" << gen_time << " data time: " << dt_time << " Esc time " << esc_time);
 
       quadrotor_msgs::MotorRPM speed;
       int16_t *rpm = snav_cached_data_struct->esc_raw.rpm;
