9fc7d00c7f9fe306167f1ffaf041fa11302d8e38
Kashish Garg <kashishgarg9@gmail.com>
2023-11-16 12:28:35 -0500

    Update function is filled out

--- trackers/kr_trackers/src/line_tracker_min_jerk_server_ros2.cpp
+++ trackers/kr_trackers/src/line_tracker_min_jerk_server_ros2.cpp
@@ -65,6 +65,7 @@ class LineTrackerMinJerk : public rclcpp::Node, public kr_trackers_manager::Trac
   bool traj_start_set_;
 
   float current_traj_length_;
+  std::shared_ptr<LineTrackerGoalHandle> goal_handle_;
 };
 
 LineTrackerMinJerk::LineTrackerMinJerk(const rclcpp::NodeOptions & options)
@@ -161,223 +162,239 @@ void LineTrackerMinJerk::Deactivate(void)
 kr_mav_msgs::msg::PositionCommand::SharedPtr LineTrackerMinJerk::update(const nav_msgs::msg::Odometry::SharedPtr &msg)
 {
 //   // Record distance between last position and current.
-//   const float dx =
-//       Eigen::Vector3f((current_pos_(0) - msg->pose.pose.position.x), (current_pos_(1) - msg->pose.pose.position.y),
-//                       (current_pos_(2) - msg->pose.pose.position.z))
-//           .norm();
-// 
-//   current_pos_(0) = msg->pose.pose.position.x;
-//   current_pos_(1) = msg->pose.pose.position.y;
-//   current_pos_(2) = msg->pose.pose.position.z;
-// 
-//   pos_set_ = true;
-//   ICs_.set_from_odom(msg);
-// 
-//   const ros::Time t_now = ros::Time::now();
-// 
-//   if(!active_)
+  const float dx =
+      Eigen::Vector3f((current_pos_(0) - msg->pose.pose.position.x), (current_pos_(1) - msg->pose.pose.position.y),
+                      (current_pos_(2) - msg->pose.pose.position.z))
+          .norm();
+
+  current_pos_(0) = msg->pose.pose.position.x;
+  current_pos_(1) = msg->pose.pose.position.y;
+  current_pos_(2) = msg->pose.pose.position.z;
+
+  pos_set_ = true;
+  ICs_.set_from_odom(msg);
+
+  const rclcpp::Time t_now = this->now();
+
+  if(!active_)
+  {
+    return kr_mav_msgs::msg::PositionCommand::SharedPtr();
+  }
+
+  current_traj_length_ += dx;
+
+  kr_mav_msgs::msg::PositionCommand::SharedPtr cmd(new kr_mav_msgs::msg::PositionCommand);
+  cmd->header.stamp = t_now;
+  cmd->header.frame_id = msg->header.frame_id;
+
+  if(goal_set_)
+  {
+    if(!traj_start_set_)
+      traj_start_ = t_now;
+
+    bool duration_set = false;
+    traj_duration_ = 0.5f;
+
+    // TODO: This should probably be after traj_duration_ is determined
+    if(goal_duration_.seconds() > traj_duration_)
+    {
+      traj_duration_ = goal_duration_.seconds();
+      duration_set = true;
+    }
+
+    // Min-Jerk trajectory
+    const float total_dist = (goal_ - ICs_.pos()).norm();
+    const Eigen::Vector3f dir = (goal_ - ICs_.pos()) / total_dist;
+    const float vel_proj = (ICs_.vel()).dot(dir);
+
+    const float t_ramp = (v_des_ - vel_proj) / a_des_;
+
+    const float distance_to_v_des = vel_proj * t_ramp + 0.5f * a_des_ * t_ramp * t_ramp;
+    const float distance_v_des_to_stop = 0.5f * v_des_ * v_des_ / a_des_;
+
+    const float ramping_distance = distance_to_v_des + distance_v_des_to_stop;
+
+    if(!duration_set)  // If duration is not set by the goal callback
+    {
+      if(total_dist > ramping_distance)
+      {
+        float t = (v_des_ - vel_proj) / a_des_                // Ramp up
+                  + (total_dist - ramping_distance) / v_des_  // Constant velocity
+                  + v_des_ / a_des_;                          // Ramp down
+
+        traj_duration_ = std::max(traj_duration_, t);
+      }
+      else
+      {
+        // In this case, v_des_ is not reached. Assume bang bang acceleration.
+
+        float vo = vel_proj;
+        float distance_to_stop = 0.5f * vo * vo / a_des_;
+
+        float t_dir;  // The time required for the component along dir
+        if(vo > 0.0f && total_dist < distance_to_stop)
+        {
+          // Currently traveling towards the goal and need to overshoot
+
+          t_dir = vo / a_des_ + std::sqrt(2.0f) * std::sqrt(vo * vo - 2.0f * a_des_ * total_dist) / a_des_;
+        }
+        else
+        {
+          // Ramp up to a velocity towards the goal before ramping down
+
+          t_dir = -vo / a_des_ + std::sqrt(2.0f) * std::sqrt(vo * vo + 2.0f * a_des_ * total_dist) / a_des_;
+        }
+        traj_duration_ = std::max(traj_duration_, t_dir);
+
+        // The velocity component orthogonal to dir
+        float v_ortho = (ICs_.vel() - dir * vo).norm();
+        float t_non_dir = v_ortho / a_des_                       // Ramp to zero velocity
+                          + std::sqrt(2.0f) * v_ortho / a_des_;  // Get back to the dir line
+
+        traj_duration_ = std::max(traj_duration_, t_non_dir);
+      }
+    }
+
+    // Find shortest angle and direction to go from yaw_ to goal_yaw_
+    float yaw_dist, yaw_dir;
+    yaw_dist = goal_yaw_ - ICs_.yaw();
+    const float pi(M_PI);  // Defined so as to force float type
+    yaw_dist = std::fmod(yaw_dist, 2 * pi);
+    if(yaw_dist > pi)
+      yaw_dist -= 2 * pi;
+    else if(yaw_dist < -pi)
+      yaw_dist += 2 * pi;
+    yaw_dir = (yaw_dist >= 0) ? 1 : -1;
+    yaw_dist = std::abs(yaw_dist);
+    goal_yaw_ = ICs_.yaw() + yaw_dir * yaw_dist;
+
+    // Consider yaw in the trajectory duration
+    if(!duration_set)  // Only if duration is not set from goal
+    {
+      if(yaw_dist > yaw_v_des_ * yaw_v_des_ / yaw_a_des_)
+        traj_duration_ = std::max(traj_duration_, yaw_dist / yaw_v_des_ + yaw_v_des_ / yaw_a_des_);
+      else
+        traj_duration_ = std::max(traj_duration_, 2 * std::sqrt(yaw_dist / yaw_a_des_));
+    }
+
+    // TODO: Should consider yaw_dot_. See hover() in MAVManager
+
+    gen_trajectory(ICs_.pos(), goal_, ICs_.vel(), Eigen::Vector3f::Zero(), ICs_.acc(), Eigen::Vector3f::Zero(),
+                   ICs_.yaw(), goal_yaw_, ICs_.yaw_dot(), 0, traj_duration_, coeffs_, yaw_coeffs_);
+
+    goal_set_ = false;
+  }
+  else if(goal_reached_)
+  {
+//   if(tracker_server_->isActive())
 //   {
-//     return kr_mav_msgs::PositionCommand::Ptr();
+//     ROS_ERROR("LineTrackerDistance::update: Action server not completed.\n");
 //   }
-// 
-//   current_traj_length_ += dx;
-// 
-//   kr_mav_msgs::PositionCommand::Ptr cmd(new kr_mav_msgs::PositionCommand);
-//   cmd->header.stamp = t_now;
-//   cmd->header.frame_id = msg->header.frame_id;
-// 
-//   if(goal_set_)
-//   {
-//     if(!traj_start_set_)
-//       traj_start_ = t_now;
-// 
-//     bool duration_set = false;
-//     traj_duration_ = 0.5f;
-// 
-//     // TODO: This should probably be after traj_duration_ is determined
-//     if(goal_duration_.toSec() > traj_duration_)
-//     {
-//       traj_duration_ = goal_duration_.toSec();
-//       duration_set = true;
-//     }
-// 
-//     // Min-Jerk trajectory
-//     const float total_dist = (goal_ - ICs_.pos()).norm();
-//     const Eigen::Vector3f dir = (goal_ - ICs_.pos()) / total_dist;
-//     const float vel_proj = (ICs_.vel()).dot(dir);
-// 
-//     const float t_ramp = (v_des_ - vel_proj) / a_des_;
-// 
-//     const float distance_to_v_des = vel_proj * t_ramp + 0.5f * a_des_ * t_ramp * t_ramp;
-//     const float distance_v_des_to_stop = 0.5f * v_des_ * v_des_ / a_des_;
-// 
-//     const float ramping_distance = distance_to_v_des + distance_v_des_to_stop;
-// 
-//     if(!duration_set)  // If duration is not set by the goal callback
-//     {
-//       if(total_dist > ramping_distance)
-//       {
-//         float t = (v_des_ - vel_proj) / a_des_                // Ramp up
-//                   + (total_dist - ramping_distance) / v_des_  // Constant velocity
-//                   + v_des_ / a_des_;                          // Ramp down
-// 
-//         traj_duration_ = std::max(traj_duration_, t);
-//       }
-//       else
-//       {
-//         // In this case, v_des_ is not reached. Assume bang bang acceleration.
-// 
-//         float vo = vel_proj;
-//         float distance_to_stop = 0.5f * vo * vo / a_des_;
-// 
-//         float t_dir;  // The time required for the component along dir
-//         if(vo > 0.0f && total_dist < distance_to_stop)
-//         {
-//           // Currently traveling towards the goal and need to overshoot
-// 
-//           t_dir = vo / a_des_ + std::sqrt(2.0f) * std::sqrt(vo * vo - 2.0f * a_des_ * total_dist) / a_des_;
-//         }
-//         else
-//         {
-//           // Ramp up to a velocity towards the goal before ramping down
-// 
-//           t_dir = -vo / a_des_ + std::sqrt(2.0f) * std::sqrt(vo * vo + 2.0f * a_des_ * total_dist) / a_des_;
-//         }
-//         traj_duration_ = std::max(traj_duration_, t_dir);
-// 
-//         // The velocity component orthogonal to dir
-//         float v_ortho = (ICs_.vel() - dir * vo).norm();
-//         float t_non_dir = v_ortho / a_des_                       // Ramp to zero velocity
-//                           + std::sqrt(2.0f) * v_ortho / a_des_;  // Get back to the dir line
-// 
-//         traj_duration_ = std::max(traj_duration_, t_non_dir);
-//       }
-//     }
-// 
-//     // Find shortest angle and direction to go from yaw_ to goal_yaw_
-//     float yaw_dist, yaw_dir;
-//     yaw_dist = goal_yaw_ - ICs_.yaw();
-//     const float pi(M_PI);  // Defined so as to force float type
-//     yaw_dist = std::fmod(yaw_dist, 2 * pi);
-//     if(yaw_dist > pi)
-//       yaw_dist -= 2 * pi;
-//     else if(yaw_dist < -pi)
-//       yaw_dist += 2 * pi;
-//     yaw_dir = (yaw_dist >= 0) ? 1 : -1;
-//     yaw_dist = std::abs(yaw_dist);
-//     goal_yaw_ = ICs_.yaw() + yaw_dir * yaw_dist;
-// 
-//     // Consider yaw in the trajectory duration
-//     if(!duration_set)  // Only if duration is not set from goal
-//     {
-//       if(yaw_dist > yaw_v_des_ * yaw_v_des_ / yaw_a_des_)
-//         traj_duration_ = std::max(traj_duration_, yaw_dist / yaw_v_des_ + yaw_v_des_ / yaw_a_des_);
-//       else
-//         traj_duration_ = std::max(traj_duration_, 2 * std::sqrt(yaw_dist / yaw_a_des_));
-//     }
-// 
-//     // TODO: Should consider yaw_dot_. See hover() in MAVManager
-// 
-//     gen_trajectory(ICs_.pos(), goal_, ICs_.vel(), Eigen::Vector3f::Zero(), ICs_.acc(), Eigen::Vector3f::Zero(),
-//                    ICs_.yaw(), goal_yaw_, ICs_.yaw_dot(), 0, traj_duration_, coeffs_, yaw_coeffs_);
-// 
-//     goal_set_ = false;
-//   }
-//   else if(goal_reached_)
-//   {
-//     if(tracker_server_->isActive())
-//     {
-//       ROS_ERROR("LineTrackerDistance::update: Action server not completed.\n");
-//     }
-// 
-//     cmd->position.x = goal_(0), cmd->position.y = goal_(1), cmd->position.z = goal_(2);
-//     cmd->yaw = goal_yaw_;
-//     cmd->yaw_dot = 0;
-//     cmd->velocity.x = 0, cmd->velocity.y = 0, cmd->velocity.z = 0;
-//     cmd->acceleration.x = 0, cmd->acceleration.y = 0, cmd->acceleration.z = 0;
-// 
-//     ICs_.set_from_cmd(cmd);
-//     return cmd;
-//   }
-// 
-//   Eigen::Vector3f x(ICs_.pos()), v(Eigen::Vector3f::Zero()), a(Eigen::Vector3f::Zero()), j(Eigen::Vector3f::Zero());
-//   float yaw_des(ICs_.yaw()), yaw_dot_des(0);
-// 
-//   const float traj_time = (t_now - traj_start_).toSec();
-// 
-//   if(traj_time >= traj_duration_)  // Reached goal
-//   {
-//     // Send a success message and reset the length and duration variables.
-//     kr_tracker_msgs::LineTrackerResult result;
-//     result.duration = traj_time;
-//     result.length = current_traj_length_;
-//     result.x = goal_(0);
-//     result.y = goal_(1);
-//     result.z = goal_(2);
-//     result.yaw = goal_yaw_;
-// 
-//     tracker_server_->setSucceeded(result);
-// 
-//     current_traj_length_ = 0.0;
-// 
-//     ROS_DEBUG_THROTTLE(1, "Reached goal");
-//     j = Eigen::Vector3f::Zero();
-//     a = Eigen::Vector3f::Zero();
-//     v = Eigen::Vector3f::Zero();
-//     x = goal_;
-//     yaw_des = goal_yaw_;
-//     yaw_dot_des = 0;
-//     goal_reached_ = true;
-//   }
-//   else if(traj_time >= 0)
-//   {
-//     float t = traj_time / traj_duration_, t2 = t * t, t3 = t2 * t, t4 = t3 * t, t5 = t4 * t;
-// 
-//     x = coeffs_[0] + t * coeffs_[1] + t2 * coeffs_[2] + t3 * coeffs_[3] + t4 * coeffs_[4] + t5 * coeffs_[5];
-//     v = coeffs_[1] + 2 * t * coeffs_[2] + 3 * t2 * coeffs_[3] + 4 * t3 * coeffs_[4] + 5 * t4 * coeffs_[5];
-//     a = 2 * coeffs_[2] + 6 * t * coeffs_[3] + 12 * t2 * coeffs_[4] + 20 * t3 * coeffs_[5];
-//     j = 6 * coeffs_[3] + 24 * t * coeffs_[4] + 60 * t2 * coeffs_[5];
-// 
-//     yaw_des = yaw_coeffs_[0] + t * yaw_coeffs_[1] + t2 * yaw_coeffs_[2] + t3 * yaw_coeffs_[3];
-//     yaw_dot_des = yaw_coeffs_[1] + 2 * t * yaw_coeffs_[2] + 3 * t2 * yaw_coeffs_[3];
-// 
-//     // Scale based on the trajectory duration
-//     v = v / traj_duration_;
-//     a = a / (traj_duration_ * traj_duration_);
-//     j = j / (traj_duration_ * traj_duration_ * traj_duration_);
-//     yaw_dot_des = yaw_dot_des / traj_duration_;
-//   }
-//   else  // (traj_time < 0) can happen when t_start is set
-//     ROS_INFO_THROTTLE(1, "Trajectory hasn't started yet");
-// 
-//   cmd->position.x = x(0), cmd->position.y = x(1), cmd->position.z = x(2);
-//   cmd->yaw = yaw_des;
-//   cmd->yaw_dot = yaw_dot_des;
-//   cmd->velocity.x = v(0), cmd->velocity.y = v(1), cmd->velocity.z = v(2);
-//   cmd->acceleration.x = a(0), cmd->acceleration.y = a(1), cmd->acceleration.z = a(2);
-//   cmd->jerk.x = j(0), cmd->jerk.y = j(1), cmd->jerk.z = j(2);
-// 
-//   ICs_.set_from_cmd(cmd);
-// 
-//   if(!goal_reached_)
-//   {
-//     kr_tracker_msgs::LineTrackerFeedback feedback;
-//     feedback.distance_from_goal = (current_pos_ - goal_).norm();
-//     tracker_server_->publishFeedback(feedback);
-//   }
-// 
-//   return cmd;
+     if (goal_handle_->get_goal() != NULL){
+       RCLCPP_ERROR_STREAM(this->get_logger(), "LineTrackerDistance::update: Action server not completed.\n");
+     }
+ 
+     cmd->position.x = goal_(0), cmd->position.y = goal_(1), cmd->position.z = goal_(2);
+     cmd->yaw = goal_yaw_;
+     cmd->yaw_dot = 0;
+     cmd->velocity.x = 0, cmd->velocity.y = 0, cmd->velocity.z = 0;
+     cmd->acceleration.x = 0, cmd->acceleration.y = 0, cmd->acceleration.z = 0;
+ 
+     ICs_.set_from_cmd(cmd);
+     return cmd;
+  }
+ 
+  Eigen::Vector3f x(ICs_.pos()), v(Eigen::Vector3f::Zero()), a(Eigen::Vector3f::Zero()), j(Eigen::Vector3f::Zero());
+  float yaw_des(ICs_.yaw()), yaw_dot_des(0);
+ 
+  //const float traj_time = (t_now - traj_start_).toSec();
+  const float traj_time = (t_now - traj_start_).seconds();
+ 
+  if(traj_time >= traj_duration_)  // Reached goal
+  {
+    // Send a success message and reset the length and duration variables.
+    kr_trackers_msgs::action::LineTracker::Result::SharedPtr result;
+    result->duration = traj_time;
+    result->length = current_traj_length_;
+    result->x = goal_(0);
+    result->y = goal_(1);
+    result->z = goal_(2);
+    result->yaw = goal_yaw_;
+ 
+    goal_handle_->succeed(result);
+ 
+    current_traj_length_ = 0.0;
+ 
+    auto& clk = *this->get_clock();
+    RCLCPP_DEBUG_THROTTLE(this->get_logger(),
+                          clk,
+                          1,
+                          "Reached goal");
+    j = Eigen::Vector3f::Zero();
+    a = Eigen::Vector3f::Zero();
+    v = Eigen::Vector3f::Zero();
+    x = goal_;
+    yaw_des = goal_yaw_;
+    yaw_dot_des = 0;
+    goal_reached_ = true;
+  }
+  else if(traj_time >= 0)
+  {
+    float t = traj_time / traj_duration_, t2 = t * t, t3 = t2 * t, t4 = t3 * t, t5 = t4 * t;
+
+    x = coeffs_[0] + t * coeffs_[1] + t2 * coeffs_[2] + t3 * coeffs_[3] + t4 * coeffs_[4] + t5 * coeffs_[5];
+    v = coeffs_[1] + 2 * t * coeffs_[2] + 3 * t2 * coeffs_[3] + 4 * t3 * coeffs_[4] + 5 * t4 * coeffs_[5];
+    a = 2 * coeffs_[2] + 6 * t * coeffs_[3] + 12 * t2 * coeffs_[4] + 20 * t3 * coeffs_[5];
+    j = 6 * coeffs_[3] + 24 * t * coeffs_[4] + 60 * t2 * coeffs_[5];
+
+    yaw_des = yaw_coeffs_[0] + t * yaw_coeffs_[1] + t2 * yaw_coeffs_[2] + t3 * yaw_coeffs_[3];
+    yaw_dot_des = yaw_coeffs_[1] + 2 * t * yaw_coeffs_[2] + 3 * t2 * yaw_coeffs_[3];
+
+    // Scale based on the trajectory duration
+    v = v / traj_duration_;
+    a = a / (traj_duration_ * traj_duration_);
+    j = j / (traj_duration_ * traj_duration_ * traj_duration_);
+    yaw_dot_des = yaw_dot_des / traj_duration_;
+  }
+  else 
+  {  // (traj_time < 0) can happen when t_start is set
+    auto& clk = *this->get_clock();
+    RCLCPP_INFO_THROTTLE(this->get_logger(),
+                          clk,
+                          1,
+                          "Trajectory hasn't started yet");
+  }
+  cmd->position.x = x(0), cmd->position.y = x(1), cmd->position.z = x(2);
+  cmd->yaw = yaw_des;
+  cmd->yaw_dot = yaw_dot_des;
+  cmd->velocity.x = v(0), cmd->velocity.y = v(1), cmd->velocity.z = v(2);
+  cmd->acceleration.x = a(0), cmd->acceleration.y = a(1), cmd->acceleration.z = a(2);
+  cmd->jerk.x = j(0), cmd->jerk.y = j(1), cmd->jerk.z = j(2);
+
+  ICs_.set_from_cmd(cmd);
+
+  if(!goal_reached_)
+  {
+    kr_trackers_msgs::action::LineTracker::Feedback::SharedPtr feedback;
+    feedback->distance_from_goal = (current_pos_ - goal_).norm();
+    goal_handle_->publish_feedback(feedback);
+  }
+
+  return cmd;
 }
 
 void LineTrackerMinJerk::goal_callback(const std::shared_ptr<LineTrackerGoalHandle> goal_handle)
 {
+  this->goal_handle_ = goal_handle;
 //  // If another goal is already active, cancel that goal
 //  // and track this one instead.
 //  if(tracker_server_->isActive())
 //  {
 //    ROS_INFO("LineTrackerMinJerk goal (%f, %f, %f) aborted.", goal_(0), goal_(1), goal_(2));
 //    tracker_server_->setAborted();
+//    goal_handle_ = goal_handle;
 //  }
+  
 //
 //  // Pointer to the goal recieved.
 //  const auto msg = tracker_server_->acceptNewGoal();
